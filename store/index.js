082a6eea55b045523a834da37f224d5e9bf5d164
Adam Clarke <9417231+Adam-Clrk@users.noreply.github.com>
2021-11-13 18:01:33 +0000

    balls

---
+++ store/index.js
@@ -0,0 +1,32 @@
+const WebSocket = require('isomorphic-ws')
+
+export const state = () => ({
+    ws: null,
+    wsAddress: 'ws://localhost:49122',
+    counter: 0,
+    state: {}
+})
+
+export const mutations = {
+    increment(state) {
+        state.counter++
+    },
+    newWs(state, ws) {
+        state.ws = ws
+    }
+}
+
+export const actions = {
+    async initWebsocket({commit, state, dispatch}) {
+        commit('newWs', new WebSocket(state.wsAddress))
+        state.ws.onclose = (e) => console.log('onclose', e)
+        state.ws.onerror = (e) => console.log('onerror', e)
+        state.ws.onopen = (e) => console.log('onopen', e)
+        state.ws.onmessage = (e) => dispatch('processMessage', e)
+    },
+    async processMessage({commit}, message) {
+        console.log('message', message)
+        
+    }
+
+}
