913b38ba078c94ec12e3f0dd69778fcd090899f8
shawmc123 <shawmc@hotmail.co.uk>
2021-11-14 00:00:58 +0000

    playerboard finished

--- components/Playerboard.vue
+++ components/Playerboard.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="w-screen">
-    <div class="flex-justify-between w-1/2 text-white sm:px-6 font-mono antialiased sm:px-6 bg-gradient-to-r from-blue-800 via-blue-400 to-blue-200 border-4 border-blue-500">
-     <span class="text-2xl pr-8 bg-gradient-to-r from blue-700 via-blue-600 to-blue-500 bg-clip-border bg-opacity-50"> {{name}}</span> 
-     <span class="font-extrabold text-xl font-mono pl-4">{{score}}</span> 
-     <span class="font-extrabold text-lg font-mono">Score</span> 
-     <span class="font-extrabold text-xl font-mono pl-4">{{goals}}</span> 
-     <span class="font-extrabold text-lg font-mono">Goals </span>
-     <span class="font-extrabold text-xl font-mono pl-4">{{shots}}</span> 
-     <span class="font-extrabold text-lg font-mono"> Shots  </span>
-     <span class="font-extrabold text-xl font-mono pl-4">{{assists}}</span> 
-     <span class="font-extrabold text-lg font-mono">Assists</span>
-    </div>
+    <div class="flex justify-between w-1/2 text-white font-mono antialiased sm:px-6 bg-gradient-to-r from-blue-800 via-blue-400 to-blue-200 border-4 border-blue-500">
+     <div class="text-2xl pr-8 bg-gradient-to-r from blue-700 via-blue-600 to-blue-500 bg-clip-border bg-opacity-50"> {{name}}</div> 
+     <div>
+      <span class="font-extrabold text-xl font-mono pl-4">{{score}}</span> 
+      <span class="font-extrabold text-lg font-mono">Score</span> 
+     </div>
+     <div>
+      <span class="font-extrabold text-xl font-mono pl-4">{{goals}}</span> 
+      <span class="font-extrabold text-lg font-mono">Goals </span>
+     </div>
+     <div>
+      <span class="font-extrabold text-xl font-mono pl-4">{{shots}}</span> 
+      <span class="font-extrabold text-lg font-mono">Shots</span>
+     </div>
+     <div>
+      <span class="font-extrabold text-xl font-mono pl-4">{{assists}}</span> 
+      <span class="font-extrabold text-lg font-mono">Assists</span>
+     </div>
+     </div>
   </div>
 </template>
 
@@ -18,10 +26,10 @@
 export default {
   props: {
     name: {default:'', type:String},
-    goals: {default:'', type:Number},
+    goals: {default:0, type:Number},
     assists: {default:'', type:String},
     score: {default:'', type:String},
-    name: {default:'', type:String},
+    shots: {default:0, type:String},
     name: {default:'', type:String},
 
   }
