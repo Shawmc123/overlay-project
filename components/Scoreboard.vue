64daf769be98f5be7e7b7e0dd24be7899f55b805
Jaddison011 <ja3g20@soton.ac.uk>
2021-11-14 00:14:19 +0000

    Added time to scoreboard and fixed alignment

--- components/Scoreboard.vue
+++ components/Scoreboard.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="mt-3 bg-gradient-to-l from-pink-600 via-indigo-400 to-blue-500 w-60">
-    <h1>
+  <div class="flex justify-between font-mono shadow-2xl text-gray-800 mt-3 bg-gradient-to-r from-blue-800 via-gray-200 to-yellow-500 w-72 border-4 rounded-xl border-gray-200">
+    <span class="pl-5 pt-0.5 pb-0.5">
+      {{team0}}
+    </span>
+    <span class="flex justify-between w-24">
+      <span class="p-0.5">
+        {{score0}}
+      </span>
+      <span class="p-0.5">
+        {{time}}
+      </span>
+      <span class="p-0.5">
+        {{score1}}
+      </span>
+    </span>
+    <span class="pr-5 pt-0.5 pb-0.5">
       {{team1}}
-      {{score1}}
-      {{score2}}
-      {{team2}}
-    </h1>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  //name: "Scoreboard"
   props: {
-    team1: {default:'', type:String},
+    team0: {default:'', type:String},
+    score0: {default:'', type:String},
     score1: {default:'', type:String},
-    score2: {default:'', type:String},
-    team2: {default:'', type:String}
+    team1: {default:'', type:String},
+    time: {default:'', type:Number}
   }
 }
 </script>
